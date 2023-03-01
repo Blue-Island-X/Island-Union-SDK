4b98e3e26d49a99a7f15e2f450018fd5ff16a136
Henry Wu <henrywu0103@protonmail.com>
2023-03-01 11:17:22 +0800

    [Optimize] Omit Kaola AppKey Property

--- src/platforms/kaola/kaola.interface.ts
+++ src/platforms/kaola/kaola.interface.ts
@@ -1,6 +1,6 @@
 import { ClientConfig } from '../../common/interfaces';
 
-export interface KaolaClientConfig extends ClientConfig {
+export interface KaolaClientConfig extends Omit<ClientConfig, 'appKey'> {
     /**
      * @param {unionId} 推广者 Id
      * 必选
